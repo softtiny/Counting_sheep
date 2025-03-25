cfc0285602fbbbf2e78316d50a075a5210c6acc2
softtiny <yingxiancapatin@gmail.com>
2025-03-25 10:03:48 +0800

    :construction: Work in progress.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "proxy.kunkka.tts"
-        minSdk = 31
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
