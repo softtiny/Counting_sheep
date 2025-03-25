e5d91c5f70c50a38d36bbf53cef0d48f0a897633
softtiny <yingxiancapatin@gmail.com>
2025-03-25 14:40:35 +0800

    :construction: Work in progress.

--- app/src/main/java/proxy/kunkka/tts/MainActivity.kt
+++ app/src/main/java/proxy/kunkka/tts/MainActivity.kt
@@ -126,19 +126,3 @@ fun TTSContent(
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TTSGoTheme {
-        Greeting("Android")
-    }
-}
