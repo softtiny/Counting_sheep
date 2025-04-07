fc6ba6d2307ab5f064f096bd2a9208be0ee56186
softtiny <yingxiancapatin@gmail.com>
2025-04-07 14:44:38 +0800

    :construction: Work in progress.

--- app/src/androidTest/java/proxy/kunkka/tts/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/proxy/kunkka/tts/ExampleInstrumentedTest.kt
@@ -11,6 +11,10 @@ import org.junit.runner.RunWith
 
 import org.junit.Assert.*
 
+
+import com.github.javiersantos.appupdater.AppUpdater
+import com.github.javiersantos.appupdater.enums.UpdateFrom
+
 /**
  * Instrumented test, which will execute on an Android device.
  *
@@ -18,6 +22,28 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @Test 
+    fun useAppUpdate() {
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        val appUpdaterUtils = AppUpdaterUtils(appContext)
+            .setUpdateFrom(UpdateFrom.JSON)
+            .setUpdateJSON("https://github.com/softtiny/Counting_sheep/releases/latest/download/update-changelog.json")
+            .withListener(object: AppUpdaterUtils.UpdateListener {
+                override fun onSuccess(update: Update, isUpdateAvailable: Boolean ) {
+                    Log.d("Latest Version", update.getLatestVersion())
+                    Log.d("Latest Version Code", update.getLatestVersionCode())
+                    Log.d("Release notes", update.getReleaseNotes())
+                    Log.d("URL", update.getUrlToDownload())
+                    Log.d("Is update available?", isUpdateAvailable.toString())
+                }
+                
+                override fun onFailed(error: AppUpdaterError) {
+                    Log.d("AppUpdater Error", "Something went wrong")
+                }
+            })
+            .start()
+        Log.i("ExampleInstrumentedTest","run useAppUpdate use context end")
+    }
     @Test
     fun useAppContext() {
         Log.i("ExampleInstrumentedTest", "run Example Instrumented Test use App context start")
@@ -25,5 +51,20 @@ class ExampleInstrumentedTest {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("proxy.kunkka.tts", appContext.packageName)
         Log.i("ExampleInstrumentedTest","run Example Instrumented Test use App context ok")
+        try {
+            Log.i("ExampleInstrumentedTest","run AppUpdater App use context")
+            AppUpdater(appContext)
+                //.setUpdateFrom(UpdateFrom.GITHUB)
+                //.setGitHubUserAndRepo("softtiny", "Counting_sheep")
+                .setUpdateFrom(UpdateFrom.JSON)
+                .setUpdateJSON("https://github.com/softtiny/Counting_sheep/releases/latest/download/update-changelog.json")
+                .start()
+            Log.i("ExampleInstrumentedTest","run AppUpdater App use context end")
+        } catch (e: Exception) {
+            // Log the error
+            e.printStackTrace()
+            // Show a toast message with error information
+            Toast.makeText(this, "Failed to check for updates: ${e.message}", Toast.LENGTH_LONG).show()
+        }
     }
 }
