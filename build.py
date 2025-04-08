c161be4ae31b927059b88c1f5d7957337a048cde
softtiny <yingxiancapatin@gmail.com>
2025-04-08 18:35:57 +0800

    :construction: Work in progress.

--- build.py
+++ build.py
@@ -1,11 +1,20 @@
 import json
 import datetime
+import sys
 
 with open("./old-update.json") as old:
     data = json.load(old)
 
+diff = 1
+args = sys.argv
+if len(args) > 1:
+    val = args[-1]
+    diff = int(val)
+
 data['latestVersion']="1.0." + datetime.datetime.strftime(datetime.datetime.now(),'%Y%m%d-%H%M%S')
-data['latestVersionCode'] +=1
+data['latestVersionCode'] +=diff
+if data['latestVersionCode'] <=0:
+    data['latestVersionCode'] = 1
 data['url']='https://github.com/softtiny/Counting_sheep/releases/latest/download/' + data['latestVersion'] + ".apk"
 
 with open("./update.json","w+") as update:
